84eb62d3608df62a39df42933d14bb73fc083923
TfRocio <chiotorresf@gmail.com>
2024-02-06 19:00:50 -0600

    Change of footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => (
   <footer className='absolute bottom-4 flex flex-col items-center w-[100%]'>
     <p className='text-xs font-medium text-[#E93A90]'>
-      Website by Rocío Torres ↩  
+      Website by Rodrigo Armenta, Teresita Juan and Rocío Torres,  ↩  
     </p>
   </footer>
 )
